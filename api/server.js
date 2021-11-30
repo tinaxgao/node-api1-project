3a007c2f1e9a8096bd94b1fa844647b9d1c46605
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-30 02:18:56 -0500

    delete user

--- api/server.js
+++ api/server.js
@@ -4,6 +4,26 @@ const User = require("./users/model");
 const server = express();
 server.use(express.json());
 
+server.delete("/api/users/:id", async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      const deletedUser = await User.remove(possibleUser.id);
+      res.status(200).json(deletedUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The user could not be removed",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
+
 server.post("/api/users", (req, res) => {
   const user = req.body;
   if (!user.name || !user.bio) {
@@ -12,10 +32,10 @@ server.post("/api/users", (req, res) => {
     });
   } else {
     User.insert(user)
-    .then(createdUser => {
-        res.status(201).json(createdUser)
-    })
-    .catch((err) => {
+      .then((createdUser) => {
+        res.status(201).json(createdUser);
+      })
+      .catch((err) => {
         res.status(500).json({
           message: "error creating user",
           err: err.message,
