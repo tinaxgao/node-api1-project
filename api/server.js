ea2ff22ee4c17b1bd6ac12b08c8a95ad54e233dd
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-30 02:32:57 -0500

    mvp

--- api/server.js
+++ api/server.js
@@ -4,6 +4,32 @@ const User = require("./users/model");
 const server = express();
 server.use(express.json());
 
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist",
+      });
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+          message: "Please provide name and bio for the user",
+        });
+      } else {
+        const updatedUser = await User.update(req.params.id, req.body);
+        res.status(200).json(updatedUser);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "error updating user",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
+
 server.delete("/api/users/:id", async (req, res) => {
   try {
     const possibleUser = await User.findById(req.params.id);
