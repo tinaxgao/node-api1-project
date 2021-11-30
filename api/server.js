15c3b419604ab4ce04896225ac48410911179af6
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-30 02:08:04 -0500

    post user

--- api/server.js
+++ api/server.js
@@ -2,11 +2,33 @@
 const express = require("express");
 const User = require("./users/model");
 const server = express();
+server.use(express.json());
+
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res.status(400).json({
+      message: "Please provide name and bio for the user",
+    });
+  } else {
+    User.insert(user)
+    .then(createdUser => {
+        res.status(201).json(createdUser)
+    })
+    .catch((err) => {
+        res.status(500).json({
+          message: "error creating user",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  }
+});
 
 server.get("/api/users", (req, res) => {
   User.find()
-    .then(users => {
-        res.json(users)
+    .then((users) => {
+      res.json(users);
     })
     .catch((err) => {
       res.status(500).json({
@@ -18,21 +40,24 @@ server.get("/api/users", (req, res) => {
 });
 
 server.get("/api/users/:id", (req, res) => {
-    User.findById(req.params.id)
-      .then(user => {
-        if (!user) {
-            res.status(404).json({message: "The user with the specified ID does not exist"})
-        }  else {
-        res.json(user)}
-      })
-      .catch((err) => {
-        res.status(500).json({
-          message: "error getting users",
-          err: err.message,
-          stack: err.stack,
-        });
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist" });
+      } else {
+        res.json(user);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "error getting users",
+        err: err.message,
+        stack: err.stack,
       });
-  });
+    });
+});
 
 server.use("*", (req, res) => {
   res.status(404).json({
