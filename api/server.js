fffa337aad3ebb9f4fb5106ecda66813da679b48
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-29 23:14:44 -0500

    progress

--- api/server.js
+++ api/server.js
@@ -3,6 +3,10 @@ const express = require('express')
 
 const server = express()
 
+server.get('/api/users', (req, res) => {
+    res.json('users')
+})
+
 server.use('*', (req, res) => {
     res.status(404).json({
         message: 'not found'
