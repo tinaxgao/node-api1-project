5ec64fa6f2f48ddb0783053fa230211f601043d6
tinaxgao <89364961+tinaxgao@users.noreply.github.com>
2021-11-30 01:53:21 -0500

    get by id

--- api/server.js
+++ api/server.js
@@ -17,6 +17,20 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+server.get("/api/users/:id", (req, res) => {
+    User.findById(req.params.id)
+      .then(user => {
+          res.json(user)
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "error getting users",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  });
+
 server.use("*", (req, res) => {
   res.status(404).json({
     message: "not found",
